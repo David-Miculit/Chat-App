2c7833061994626ee80220e72c6a58b94781039a
David Miculit <David.Miculit@cognizant.com>
2026-01-29 14:59:16 +0200

    fix: last message wasn't showing correctly in the chat bubble

--- scripts/server.js
+++ scripts/server.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const fs = require('fs')
 const cors = require('cors')
+const { log } = require('console')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 const MSG_PATH = './data/messages.json'
+const CONTACTS_PATH = "./data/contacts.json"
 
 app.post('/api/messages', (req,res) => {
     const {contactId,newMessage} = req.body
 
-    const data = JSON.parse(fs.readFileSync(MSG_PATH, 'utf8'))
+    const messagesData = JSON.parse(fs.readFileSync(MSG_PATH, 'utf8'))
+    if(!messagesData[contactId]) 
+        messagesData[contactId] = []
+    messagesData[contactId].push(newMessage)
+    fs.writeFileSync(MSG_PATH, JSON.stringify(messagesData, null,2))
 
-    if(!data[contactId]) 
-        data[contactId] = []
-    data[contactId].push(newMessage)
+    const contactsData = JSON.parse(fs.readFileSync(CONTACTS_PATH, 'utf8'))
+    const contactIndex = contactsData.findIndex(c => c.id === contactId)
+    if(contactIndex != -1) {
+        contactsData[contactIndex].lastMessage = newMessage.text
+        contactsData[contactIndex].lastMessageTime = newMessage.timestamp
 
-    fs.writeFileSync(MSG_PATH, JSON.stringify(data, null,2))
+        fs.writeFileSync(CONTACTS_PATH, JSON.stringify(contactsData, null,2))
+    }
+
     res.status(200).send({status:'Saved'})
 })
 
